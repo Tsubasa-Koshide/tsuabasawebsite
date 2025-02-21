b58e0ab64f5d0df2b75ca5cc390a0e532035fe33
Tsubasa-Koshide <koshidetsubasa@gmail.com>
2025-02-21 21:58:29 +0900

    sad

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/content';
-import content from '@astro/content';
+// src/content/config.ts
+import { defineCollection, z } from 'astro:content';
 
-export default defineConfig({
-  integrations: [content()],
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    slug: z.string(),
+  }),
 });
+
+export const collections = { projects };
