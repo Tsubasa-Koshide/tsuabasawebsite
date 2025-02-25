0cc6de0038d2a133be1c2fd4c3dc8f856b30169f
Tsubasa-Koshide <koshidetsubasa@gmail.com>
2025-02-25 21:57:58 +0900

    Super update

--- src/content/config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // 必要な設定や他のインテグレーションをここに記述
-});
---
+++ src/data/projects.js
@@ -0,0 +1,166 @@
+// src/data/projects.js
+
+// --- Project 01 ---
+import p01_01 from "../assets/photo/project01/01.jpeg";
+import p01_02 from "../assets/photo/project01/02.jpeg";
+import p01_03 from "../assets/photo/project01/03.jpeg";
+
+// --- Project 02 ---
+import p02_01 from "../assets/photo/project02/01.jpeg";
+import p02_02 from "../assets/photo/project02/02.jpeg";
+
+// --- Project 03 ---
+import p03_01 from "../assets/photo/project03/01.jpeg";
+import p03_03 from "../assets/photo/project03/03.jpeg";
+
+// --- Project 04 ---
+import p04_01 from "../assets/photo/project04/01.jpeg";
+import p04_02 from "../assets/photo/project04/02.jpeg";
+import p04_03 from "../assets/photo/project04/03.jpeg";
+
+// --- Project 05 ---
+import p05_01 from "../assets/photo/project05/01.jpeg";
+import p05_02 from "../assets/photo/project05/02.jpeg";
+
+// --- Project 06 ---
+import p06_01 from "../assets/photo/project06/01.jpeg";
+import p06_02 from "../assets/photo/project06/02.jpeg";
+
+// --- Project 07 ---
+import p07_01 from "../assets/photo/project07/01.jpeg";
+import p07_02 from "../assets/photo/project07/02.jpeg";
+import p07_03 from "../assets/photo/project07/03.jpeg";
+
+// --- Project 08 ---
+import p08_01 from "../assets/photo/project08/01.jpeg";
+import p08_02 from "../assets/photo/project08/02.jpeg";
+
+// --- Project 09 ---
+import p09_01 from "../assets/photo/project09/01.jpeg";
+import p09_02 from "../assets/photo/project09/02.jpeg";
+
+// --- Project 10 ---
+import p10_01 from "../assets/photo/project10/01.jpeg";
+import p10_02 from "../assets/photo/project10/02.jpeg";
+
+// --- Project 11 ---
+import p11_01 from "../assets/photo/project11/01.jpeg";
+import p11_02 from "../assets/photo/project11/02.jpeg";
+import p11_03 from "../assets/photo/project11/03.jpeg";
+import p11_04 from "../assets/photo/project11/04.jpeg";
+
+// --- Project 12 ---
+import p12_01 from "../assets/photo/project12/01.jpeg";
+import p12_02 from "../assets/photo/project12/02.jpeg";
+import p12_03 from "../assets/photo/project12/03.jpeg";
+import p12_04 from "../assets/photo/project12/04.jpeg";
+import p12_05 from "../assets/photo/project12/05.jpeg";
+
+// --- Project 13 ---
+import p13_01 from "../assets/photo/project13/01.jpeg";
+import p13_02 from "../assets/photo/project13/02.jpeg";
+import p13_03 from "../assets/photo/project13/03.jpeg";
+
+// --- Project 14 ---
+import p14_01 from "../assets/photo/project14/01.jpeg";
+import p14_02 from "../assets/photo/project14/02.jpeg";
+
+// --- Project 15 ---
+import p15_01 from "../assets/photo/project15/01.jpeg";
+import p15_02 from "../assets/photo/project15/02.jpeg";
+import p15_03 from "../assets/photo/project15/03.jpeg";
+import p15_04 from "../assets/photo/project15/04.jpeg";
+
+// エクスポートするプロジェクトの配列
+export default [
+  {
+    title: "CGI: artworks",
+    description: "self development work\n2020-2022",
+    image: [p01_01, p01_02, p01_03],
+    slug: "project01"
+  },
+  {
+    title: "concept:Future sole 2040",
+    description: "Collaboration with On running shoes\nExhibition at On Pavilion swiss\nExhibition at Ecal swiss\n2023",
+    image: [p02_01, p02_02],
+    slug: "project02"
+  },
+  {
+    title: "concept: long distance car",
+    description: "Collaboration with Santoni\nDesign with cergei cilcov\n2022",
+    image: [p03_01, p03_03],
+    slug: "project03"
+  },
+  {
+    title: "concept: Mini-nyan!",
+    description: "Collaboration with BMW mini\nExhibition at Munich Design week\n2022",
+    image: [p04_01, p04_02, p04_03],
+    slug: "project04"
+  },
+  {
+    title: "Product: Messa luna",
+    description: "Self production\nDesign with Tsuchida Kyohei\nsale in TAK STUDIO SHOP\n2022",
+    image: [p05_01, p05_02],
+    slug: "project05"
+  },
+  {
+    title: "FRAMEs: OJIYU model",
+    description: "Collaboration with OJIYU\nExhibition at Such as Gallery\n2024",
+    image: [p06_01, p06_02],
+    slug: "project06"
+  },
+  {
+    title: "FRAMEs: Komatsu Tasuku model",
+    description: "Collaboration with Tasuku Komatsu\nExhibition at PARCO Shibuya\n2023-",
+    image: [p07_01, p07_02, p07_03],
+    slug: "project07"
+  },
+  {
+    title: "FRAMEs: Takita Saki model8",
+    description: "Collaboration with Takita Saki\nClient R11R\nExhibition at PARCO Ikebukuro\n2024-",
+    image: [p08_01, p08_02],
+    slug: "project08"
+  },
+  {
+    title: "FRAMEs: millitsuka model",
+    description: "Collaboration with millitsuka\nExhibition at Isola design week, milano\n2024",
+    image: [p09_01, p09_02],
+    slug: "project09"
+  },
+  {
+    title: "FRAMEs: Arai Kiriko model",
+    description: "Collaboration with Arai Kiriko\nClient R11R\nExhibition at PARCO Ikebukuro\n2024",
+    image: [p10_01, p10_02],
+    slug: "project10"
+  },
+  {
+    title: "CGI: risograph",
+    description: "Self production\nPrint in STUDIO LE MONDE\nsale in TAK STUDIO SHOP\n2024",
+    image: [p11_01, p11_02, p11_03, p11_04],
+    slug: "project11"
+  },
+  {
+    title: "FRAMEs: millitsuka model",
+    description: "Collaboration with millitsuka\nExhibition at Sudachidou\nExhibition at PARCO Ikebukuro\nExhibition at NEWSPACE PA\n2024",
+    image: [p12_01, p12_02, p12_03, p12_04, p12_05],
+    slug: "project12"
+  },
+  {
+    title: "FRAMEs: Yoneyama Mai model",
+    description: "Collaboration with Yoneyama Mai\nClient R11R\nExhibition at PARCO Ikebukuro\n2024",
+    image: [p13_01, p13_02, p13_03],
+    slug: "project13"
+  },
+  {
+    title: "CGI: Kasumigaseki ART STREET MUESEN",
+    description: "Client JDN\nExhibition at Kasumigaseki building\n2024",
+    image: [p14_01, p14_02],
+    slug: "project14"
+  },
+  {
+    title: "FRAMEs: AAMY model",
+    description: "Collaboration with AAMY\nClient g-graphics\nExhibition at PARCO Ikebukuro\n2024",
+    image: [p15_01, p15_02, p15_03, p15_04],
+    slug: "project15"
+  }
+];
